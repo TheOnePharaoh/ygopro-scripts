22118a576e2564f55bdbc14b727a2c1860435d9d
TheOnePharaoh <TheOnePharaoh@users.noreply.github.com>
2020-01-11 18:00:02 -0600

    fixed Scrap Lube
    fixed the condition in case activated on the opponent's turn

--- c22539270.lua
+++ c22539270.lua
@@ -26,7 +26,7 @@ function c22539270.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 function c22539270.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetCurrentPhase()==PHASE_MAIN1 end
+if chk==0 then return Duel.GetActivityCount(tp,ACTIVITY_BATTLE_PHASE)==0 end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_BP)
